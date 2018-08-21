20edb7111aaaa919a6b61dec767d923d950fcee5
Ete Remi <remi.ete@desy.de>
2018-08-21 15:24:25 +0200

    Removed releases metadata treatment as this is done by package versioning directly

--- doxygen.py
+++ doxygen.py
@@ -35,23 +35,6 @@ def checkPackageVersion(data, package, version):
         return False
     
     return True
-
-
-def addRelease(data, release, packages, overwrite):
-    if not data.has_key("releases"):
-        data["releases"] = {}
-        
-    if data["releases"].has_key(release) and not overwrite:
-        raise RuntimeError("Attempt to overwrite an existing release ({0})".format(release))
-    else:
-        data["releases"][release] = {}
-    
-    for pkg, version in packages.items():
-        
-        if not checkPackageVersion(data, pkg, version):
-            raise RuntimeError("Package '{0}', version '{1}' not found in list of available packages !".format(pkg, version))
-        
-        data["releases"][release][pkg] = version
     
 
 if __name__ == "__main__" :
@@ -71,17 +54,7 @@ if __name__ == "__main__" :
     parser.add_argument("--pkg-name", action="store", default="",
                             help="The package name if the option --add-pkg is used", required=False)
     parser.add_argument("--pkg-version", action="store", default="",
-                            help="The package version if the option --add-pkg is used", required=False)
-
-    # release config
-    parser.add_argument("--add-release", action="store_true", default="",
-                            help="Add a new software release", required=False)
-    parser.add_argument("--release-version", action="store", default="",
-                            help="The new software release version", required=False)
-    parser.add_argument("--release-packages", action="store", default="", nargs='+',
-                            help="List of packages and versions to add to the release. Format is list of pkg:version", required=False)
-    parser.add_argument("--release-overwrite", action="store_true", 
-                            help="Whether to overwrite an existing release", required=False)                            
+                            help="The package version if the option --add-pkg is used", required=False)                       
                             
     
     parsed = parser.parse_args()
@@ -94,20 +67,6 @@ if __name__ == "__main__" :
         version = parsed.pkg_version
         
         addPackage(data, package, version)
-    
-    # add a new release
-    if parsed.add_release:
-        version = parsed.release_version
-        overwrite = parsed.release_overwrite
-        packages = {}
-        
-        for arg in parsed.release_packages:
-            split = arg.split(":")
-            if len(split) != 2:
-                raise RuntimeError("Option --release-packages: invalid arg '{0}', expected format pkg:version".format(arg))
-            packages[split[0]] = split[1]
-        
-        addRelease(data, version, packages, overwrite)
         
     # write out the meta data
     if parsed.output:
